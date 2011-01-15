5bf0b2afee4ae5169df2cc09a46dabfe271fd770
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2011-01-15 23:58:18 +0100

    updated and renamed the wish editor

--- wish_editor.py
+++ wish_editor.py
@@ -13,9 +13,11 @@ import sys
 import pango
 
 from __init__ import GnomeConfig
+from note_editor import NoteEditor
 from note import Note
+
 	
-class WishEditor :
+class WishEditor:
 
 	def set_active_combo(self, combobox, break_value):
 		model = combobox.get_model()
@@ -70,7 +72,6 @@ class WishEditor :
 		self.fill_in_combobox(self.slags, Slags)
 		self.fill_in_combobox(self.notes, Notes)
 	
-
 	def _init_alias(self):
 		self.editor = self.gui.get_widget("wish_editor")		
 		self.name  = self.gui.get_widget("wish_text")
@@ -91,10 +92,81 @@ class WishEditor :
 		if combo_active != None:
 			return combo_model.get_value(combo_active, 1)
 		else:
-			return None
+			return No
 
-	def response(self, widget):
+	def delete_row(self, combobox):
+		combo_value = self.get_selected_combo(combobox)
+		title = combo_value.get_title() 
+		if combo_value != None and combo_value.can_delete() and (title != "Ingen" and not (title in GnomeConfig.start_media_txt)):
+			combo_active = combobox.get_active()
+			combobox.remove_text(combo_active)
+			if len(combobox.get_model()) > 0:
+				combobox.set_active(0)
+
+  	# Notes
+  	def new_note(self, widget):
+		NoteEditor(None, self.add_new_note)
+
+	def add_new_note(self, item):
+		store = self.notes.get_model()
+		self.add_single_item(item, store)
+
+	def edit_note(self, widget):
+		item = self.get_selected_combo(self.notes)
+		NoteEditor(item, self.do_note_edit)
+	
+	def do_note_edit(self, widget):
+		print "do edit stuff"
 		
+	def remove_note(self, widget):
+		self.delete_row(self.notes)
+
+	# Types
+	def new_type(self, widget):
+		NoteEditor(None, self.add_new_type)
+		
+	def add_new_type(self, item):
+		store = self.slags.get_model()
+		self.add_single_item(item, store)
+	
+	def edit_type(self, widget):
+		item = self.get_selected_combo(self.slags)
+		NoteEditor(item, self.do_type_edit)
+
+	def do_type_edit(self, item):
+		print "do edit stuff"
+				
+	def remove_type(self, widget):
+		self.delete_row(self.slags)
+
+
+
+	def add_single_item(self, item, store):
+		store.append([item.get_title(), item])
+
+  	
+  	def _init_signal_connections(self):
+		SIGNAL_CONNECTIONS_DIC = {
+			"on_cancel": self.cancel,
+			"on_response": self.response,
+			
+            "on_new_note": self.new_note,
+			"on_edit_note": self.edit_note,
+			"on_remove_note": self.remove_note,            
+			
+			"on_new_slags": self.new_type,
+			"on_edit_slags": self.edit_type,
+			"on_remove_slags": self.remove_type,
+			
+			"on_delete_cancel": lambda x: x.hide
+        	}
+		self.gui.signal_autoconnect(SIGNAL_CONNECTIONS_DIC)
+		
+		
+  	def cancel(self, widget):
+		self.editor.destroy()
+		
+	def response(self, widget):	
 		name = self.name.get_text()
 		price = self.price.get_value()		
 
@@ -120,45 +192,3 @@ class WishEditor :
 
 		self.response([name, price, slags.title, note.title, slags, note])					
 		self.editor.destroy()		
-		
-  	def cancel(self, widget):
-		self.editor.destroy()
-
-  	def new_note(self, widget):
-		self.edit_new(self.Notes)
-
-	def remove_note(self, widget):
-		self.delete_row(self.notes)
-
-	def delete_row(self, combobox):
-		combo_value = self.get_selected_combo(combobox)
-		title = combo_value.get_title() 
-		if combo_value != None and combo_value.can_delete() and (title != "Ingen" and not (title in GnomeConfig.start_media_txt)):
-			combo_active = combobox.get_active()
-			combobox.remove_text(combo_active)
-			if len(combobox.get_model()) > 0:
-				combobox.set_active(0)
-
-	def edit_new(self, List):
-		type_edit = gtk.glade.XML(GnomeConfig.main_gui, "edit_new")
-		self.edit_combo = type_edit.get_widget("edit_combobox")	
-		self.fill_in_combobox(self.edit_combo, List)
-		print len(self.edit_combo.get_model())
-
-	def new_type(self, widget):
-		self.edit_new(self.Slags)
-		
-	def remove_type(self, widget):
-		self.delete_row(self.slags)
-
-  	def _init_signal_connections(self):
-		SIGNAL_CONNECTIONS_DIC = {
-			"on_cancel": self.cancel,
-			"on_response": self.response,
-            "on_new_note": self.new_note,
-			"on_remove_note": self.remove_note,            
-			"on_new_slags": self.new_type,
-			"on_remove_slags": self.remove_type,
-			"on_delete_cancel": lambda x: x.hide
-        	}
-		self.gui.signal_autoconnect(SIGNAL_CONNECTIONS_DIC)
